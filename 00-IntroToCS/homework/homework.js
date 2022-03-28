217dae6a06abf70aad3659535166687516bd296b
Felipe Guitelmane <felipe.guitelman@.com>
2022-03-28 18:26:40 -0300

    Homework 1 finished

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var arrey = num.split ('')
+  var revarr = []
+  for (var i = 0 ; i < arrey.length ; i++) {
+    revarr.unshift (arrey[i])
+  }
+  var decim = 0
+  for ( let i = 0 ; i < revarr.length ; i++) {
+    decim = decim + revarr[i]*(Math.pow (2,i))
+  }
+  return decim
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var defnum = ''
+    var otnum = num
+    do {
+      defnum = defnum + (otnum % 2)
+      otnum = (otnum - (otnum % 2))/2
+    } while (otnum >= 1)
+    return defnum.split('').reverse().join('')
 }
 
 
